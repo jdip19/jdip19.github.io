4855ab376fcce3233e1e3ad4a3d2f3bcefb840a2
Mohil Tharecha <jaydipupadhyay5@gmail.com>
2025-08-12 17:41:06 +0530

    2 more case study addedd

--- js/main.js
+++ js/main.js
@@ -425,7 +425,7 @@ horizontalSections.forEach(function (sec, i) {
 
     const thisPinWrap = sec.querySelector('.pin-wrap');
     const thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');
-    const getToValue = () => -(thisAnimWrap.scrollWidth - (thisAnimWrap.scrollWidth / 7));
+    const getToValue = () => -(thisAnimWrap.scrollWidth - (thisAnimWrap.scrollWidth / 9));
     const lastItemWidth = thisAnimWrap.lastElementChild.offsetWidth;
 
     gsap.fromTo(thisAnimWrap, {
